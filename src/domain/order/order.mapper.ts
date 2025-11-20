1140f2037c718c6ff7369c5989748dcac53771da
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-11-20 19:22:39 +0330

    chore: id can be undefined

--- src/domain/order/order.mapper.ts
+++ src/domain/order/order.mapper.ts
@@ -1,13 +1,14 @@
 import {OrderModel} from "../../infrastructure/db/model/order.model";
+import {Order} from "./order.entity";
 
 export class OrderMapper {
     static toPersistence(order: Order): Partial<OrderModel> {
         return {
-            id: order.id,
+            id: order.id || undefined,
             userId: order.userId,
             originToken: order.originToken,
             destinationToken: order.destinationToken,
-            amount: order.amount.toString(),
+            amount: order.amount,
             status: order.status,
         }
     }
