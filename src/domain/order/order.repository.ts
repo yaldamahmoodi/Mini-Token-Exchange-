199695aee60310aea9a3f79b74dbdc4570c27c3b
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-11-21 15:25:52 +0330

    feat: Add findFiltered method in IOrderRepository

--- src/domain/order/order.repository.ts
+++ src/domain/order/order.repository.ts
@@ -2,6 +2,8 @@ import {Order} from "./order.entity";
 
 export interface IOrderRepository {
     save(order: Order): Promise<Order>;
+
     findById(id: string): Promise<Order | null>;
-    getAll(): Promise<Order[]>;
+
+    findFiltered(filters: { userId?: string; status?: string }): Promise<Order[]>;
 }
