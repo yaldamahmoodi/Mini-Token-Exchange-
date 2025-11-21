0fdad30eee7e032802e4d939e222884bbeeea10b
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-11-21 18:44:16 +0330

    chore: use pinoLogger

--- src/presentation/shared/handle-error.ts
+++ src/presentation/shared/handle-error.ts
@@ -1,17 +1,18 @@
-import { Response } from "express";
-import { ZodError } from "zod";
-import { DomainError } from "../../domain/errors/domain-error";
+import {Response} from "express";
+import {ZodError} from "zod";
+import {DomainError} from "../../domain/errors/domain-error";
+import {pinoLogger} from "./pino-logger";
 
 export function handleError(err: any, res: Response) {
     if (err instanceof ZodError) {
         const message = err.issues[0]?.message || "Invalid input";
-        return res.status(422).json({ error: "ValidationError", message });
+        return res.status(422).json({error: "ValidationError", message});
     }
 
     if (err instanceof DomainError) {
-        return res.status(400).json({ error: err.name, message: err.message });
+        return res.status(400).json({error: err.name, message: err.message});
     }
 
-    console.error(err);
-    return res.status(500).json({ error: "InternalServerError", message: "Something went wrong" });
+    pinoLogger.info(err, "Something went wrong");
+    return res.status(500).json({error: "InternalServerError", message: "Something went wrong"});
 }
