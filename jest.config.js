384ece5f742b839038b17a60ebc5a09cf42395bd
Yalda Mahmoodi <yaldamahmoodi10@gmail.com>
2025-11-21 18:46:56 +0330

    feat: pino and jest packages added

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    verbose: true,
+};
+exports.default = config;
